cf75ce14f34bba9cd5637de33d22b2c5fc085cce
Josef Baltz <josef@baltz.net>
2018-02-13 17:51:31 -0600

    Redstone Alloy
    Redstone Alloy
    Redstone Alloy Block
    Redstone Clay

--- build/sources/main/java/me/orangeflare/vanillaexpanded/init/BlockInit.java
+++ build/sources/main/java/me/orangeflare/vanillaexpanded/init/BlockInit.java
@@ -10,5 +10,6 @@ import java.util.List;
 public class BlockInit {
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-    public static final Block BLOCK_DEV = new BlockBase("block_dev", Material.ROCK);
+    //public static final Block BLOCK_DEV = new BlockBase("block_dev", Material.CLOTH, 0.1F);
+    public static final Block REDSTONE_ALLOY_BLOCK = new BlockBase("redstone_alloy_block", Material.IRON, 3.5F);
 }
--- src/main/java/me/orangeflare/vanillaexpanded/objects/blocks/BlockBase.java
+++ src/main/java/me/orangeflare/vanillaexpanded/objects/blocks/BlockBase.java
@@ -11,12 +11,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
 public class BlockBase extends Block implements IHasModel {
-    public BlockBase(String name, Material material) {
+    public BlockBase(String name, Material material, Float hardness) {
         super(material);
         setUnlocalizedName(name);
         setRegistryName(name);
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-
+        setHardness(hardness);
         BlockInit.BLOCKS.add(this);
         ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
