afe560e768df12bae5ab158cdaf9407a4da5fc30
Josef Baltz <josef@baltz.net>
2018-02-14 07:02:03 -0600

    Unknown Changes

--- build/sources/main/java/me/orangeflare/vanillaexpanded/handlers/SmeltingHandler.java
+++ build/sources/main/java/me/orangeflare/vanillaexpanded/handlers/SmeltingHandler.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class SmeltingHandler {
     public static void registerSmeltingRecipes() {
-        GameRegistry.addSmelting(ItemInit.REDSTONE_CLAY, new ItemStack(ItemInit.REDSTONE_ALLOY_INGOT), 0f);
+        GameRegistry.addSmelting(ItemInit.REDSTONE_CLAY, new ItemStack(ItemInit.REDSTONE_ALLOY_INGOT), 1.0f);
 
     }
 }
